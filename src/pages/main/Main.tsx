a10f136df8300a58cab0aa175d8f7354eb3277d1
Svetlana <claire-An@mail.ru>
2024-05-13 22:33:40 +0500

    feat: add layout main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
 import styles from './main.module.scss';
+import imgLeft from '../../assets/image/login.png';
+import imgRight from '../../assets/image/login1.png';
+import imgCenter from '../../assets/image/registration.png';
 
 const Main: React.FC = () => {
-  useEffect(() => {});
+  // useEffect(() => {});
 
-  const registrationHandler = async () => {};
+  // const registrationHandler = async () => {};
 
   return (
-    <div className={styles.main}>
-      <h2>Main</h2>
-      <button onClick={registrationHandler}>Войти</button>
-    </div>
+    <main className={styles.main}>
+      <h2>Создадим сад в вашем доме и офисе</h2>
+      <p>Если в большом городе вы скучаете по природе, ничто не мешает вам превратить в оазис квартиру или офис. И мы поможем вам это сделать!</p>
+      <img src={imgLeft}/>
+      <img src={imgCenter}/>
+      <img src={imgRight}/>
+    </main>
   );
 };
 
