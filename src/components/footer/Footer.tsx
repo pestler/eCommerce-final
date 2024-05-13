f8b1798bc8f6636cf685492359a118ee8d1548b8
Svetlana <claire-An@mail.ru>
2024-05-13 22:07:03 +0500

    feat: add footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,50 +1,46 @@
 import React from 'react';
-import style from './footer.module.scss';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/image/logo.svg';
 import { listLinks } from '../header/listLink';
-import { Link } from 'react-router-dom';
+import style from './footer.module.scss';
 
 const productGroup = [
-  "Кактусы",
+  'Кактусы',
   'Суккуленты',
   'Бонсаи',
   'Комнатные растения',
-  'Флорариумы'
+  'Флорариумы',
 ];
 
 const team = [
   {
-    "name": "Ivan Haurylenka",
-    "gitHub": "ivankirik"
+    name: 'Ivan Haurylenka',
+    gitHub: 'Ivankirik',
   },
   {
-    "name": "Svetlana Antonova",
-    "gitHub": "claire-an"
+    name: 'Svetlana Antonova',
+    gitHub: 'Claire-An',
   },
   {
-    "name": "Aleh Kuis",
-    "gitHub": "pestler"
-  }
+    name: 'Aleh Kuis',
+    gitHub: 'Pestler',
+  },
 ];
 
 const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className={style.footer}>
       <div className={style.wrapper}>
-        <img src={logo} />
+        <img src={logo} className={style.footer__logo} />
         <div className={style.footer_menu}>
           <h3>Меню</h3>
           {listLinks.map((link) => (
-              <Link
-                key={link.id}
-                to={link.to}
-                className={style.link}
-              >
-                {link.textLink}
-              </Link>
+            <Link key={link.id} to={link.to} className={style.link}>
+              {link.textLink}
+            </Link>
           ))}
         </div>
-        <div className={style.header__group}>
+        <div className={style.footer__group}>
           <h3>КАТЕГОРИИ</h3>
           {productGroup.map((group) => {
             return (
@@ -54,11 +50,16 @@ const Footer: React.FC = () => {
             );
           })}
         </div>
-        <div>
+        <div className={style.footer__team}>
           <h3>Команда разработчиков</h3>
           {team.map((value) => {
             return (
-              <Link to={"https://github.com/" + value.gitHub} className={style.link} key={value.gitHub} title={value.name}>
+              <Link
+                to={'https://github.com/' + value.gitHub}
+                className={style.link}
+                key={value.gitHub}
+                title={value.name}
+              >
                 {value.gitHub}
               </Link>
             );
