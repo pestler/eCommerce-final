b982f3009a98d74c2028bb401c129903ea381321
Svetlana <claire-An@mail.ru>
2024-05-14 19:01:36 +0500

    feat: add main page styles

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,7 @@
-import styles from './main.module.scss';
 import imgLeft from '../../assets/image/login.png';
 import imgRight from '../../assets/image/login1.png';
 import imgCenter from '../../assets/image/registration.png';
+import styles from './main.module.scss';
 
 const Main: React.FC = () => {
   // useEffect(() => {});
@@ -9,13 +9,16 @@ const Main: React.FC = () => {
   // const registrationHandler = async () => {};
 
   return (
-    <main className={styles.main}>
-      <h2>Создадим сад в вашем доме и офисе</h2>
-      <p>Если в большом городе вы скучаете по природе, ничто не мешает вам превратить в оазис квартиру или офис. И мы поможем вам это сделать!</p>
-      <img src={imgLeft}/>
-      <img src={imgCenter}/>
-      <img src={imgRight}/>
-    </main>
+    <div className={styles.main__wrapper}>
+      <h2>Создадим сад <br/>в вашем доме и офисе</h2>
+      <p className={styles.main__description}>
+        Если в большом городе вы скучаете по природе, ничто не мешает вам
+        превратить в оазис квартиру или офис. И мы поможем вам это сделать!
+      </p>
+      <img src={imgLeft} className={styles.main__imgleft}/>
+      <img src={imgRight} className={styles.main__imgright}/>
+      <img src={imgCenter} className={styles.main__imgcenter}/>
+    </div>
   );
 };
 
