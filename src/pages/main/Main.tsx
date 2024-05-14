c6296d15002c7e59502144de9af2a0ac333ddd8b
Svetlana <claire-An@mail.ru>
2024-05-14 23:03:12 +0500

    feat: change main page styles

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -10,14 +10,16 @@ const Main: React.FC = () => {
 
   return (
     <div className={styles.main__wrapper}>
-      <h2>Создадим сад <br/>в вашем доме и офисе</h2>
+      <h2>
+        Создадим сад <br />в вашем доме и офисе
+      </h2>
       <p className={styles.main__description}>
         Если в большом городе вы скучаете по природе, ничто не мешает вам
         превратить в оазис квартиру или офис. И мы поможем вам это сделать!
       </p>
-      <img src={imgLeft} className={styles.main__imgleft}/>
-      <img src={imgRight} className={styles.main__imgright}/>
-      <img src={imgCenter} className={styles.main__imgcenter}/>
+      <img src={imgLeft} className={styles.main__imgleft} />
+      <img src={imgRight} className={styles.main__imgright} />
+      <img src={imgCenter} className={styles.main__imgcenter} />
     </div>
   );
 };
