5eb1cfb4452d10311ecdf6bb79f92014f750e87c
Aleh Kuis <pestleralex@gmail.com>
2024-05-19 17:23:20 +0300

    feat: add adaptive for registration login

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -6,6 +6,7 @@ import Button from '../../components/button/Button.tsx';
 import { RegistrationForm } from '../../interface/registrationForm.ts';
 import { customerService } from '../../services/customer.service.ts';
 import { dateValidation } from '../../validators/date-validation.ts';
+import { generalValidation } from '../../validators/general-validation.ts';
 import { loginValidation } from '../../validators/login-validation.ts';
 import { nameValidation } from '../../validators/name-validation.ts';
 import { passwordValidation } from '../../validators/password-validation.ts';
@@ -82,141 +83,148 @@ const Registration: React.FC = () => {
   };
 
   return (
-    <div className={styles.container__registration}>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <h3 className={styles.title}>Регистрация на сайте</h3>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <h3 className={styles.title}>Регистрация на сайте</h3>
 
-        <h4 className={styles.info__container}>Личная информация</h4>
-        <Input
-          {...register('firstName', nameValidation())}
-          placeholder="First Name"
-          type="text"
-          error={errors.firstName}
-        />
-        <Input
-          {...register('lastName', nameValidation())}
-          id="last-name"
-          placeholder="Last Name"
-          type="text"
-          error={errors.lastName}
-        />
-        <Input
-          {...register('dateOfBirth', dateValidation())}
-          id="date-birth"
-          type="date"
-          placeholder="dateOfBirth"
-          error={errors.dateOfBirth}
-          aria-invalid="true"
-        />
-        <Input
-          {...register('email', loginValidation())}
-          placeholder="Enter email"
-          type="text"
-          error={errors.email}
-        />
-        <Input
-          {...register('registerPassword', passwordValidation())}
-          placeholder="Enter password"
-          type="password"
-          error={errors.registerPassword}
-        />
-        <Input
-          {...register(
-            'repeatPassword',
-            repeatPasswordValidation(passwordValue),
-          )}
-          placeholder="Repeat password"
-          type="password"
-          error={errors.repeatPassword}
-        />
+      <div className={styles.container__registration}>
+        <div className={styles.container__general}>
+          <h4 className={styles.info__title}>Личная информация</h4>
 
-        <h4>Адрес для выставления счетов</h4>
-        <Input
-          {...register('billingCountry')}
-          placeholder="Country"
-          id="country"
-          error={errors.billingCountry}
-        />
-        <Input
-          {...register('billingCity')}
-          placeholder="City"
-          id="billingCity"
-          error={errors.billingCity}
-        />
-        <Input
-          {...register('billingStreet')}
-          placeholder="Stret"
-          id="billingStreet"
-          error={errors.billingStreet}
-        />
-        <Input
-          {...register('billingHouseNumber')}
-          placeholder="HouseNumber"
-          id="billingHouseNumber"
-          error={errors.billingHouseNumber}
-        />
-        <Input
-          {...register('billingApartment')}
-          placeholder="Apartment"
-          id="billingApartment"
-          error={errors.billingApartment}
-        />
-        <Input
-          {...register('billingPostcode')}
-          placeholder="Postcode"
-          id="billingPostcode"
-          error={errors.billingPostcode}
-        />
+          <Input
+            {...register('firstName', nameValidation())}
+            placeholder="First Name"
+            type="text"
+            error={errors.firstName}
+          />
+          <Input
+            {...register('lastName', nameValidation())}
+            id="last-name"
+            placeholder="Last Name"
+            type="text"
+            error={errors.lastName}
+          />
+          <Input
+            {...register('dateOfBirth', dateValidation())}
+            id="date-birth"
+            type="date"
+            placeholder="dateOfBirth"
+            error={errors.dateOfBirth}
+            aria-invalid="true"
+          />
 
-        <h4>Адрес доставки</h4>
-        <Input
-          {...register('shippingCountry')}
-          placeholder="Country"
-          id="bilingCountry"
-          error={errors.shippingCountry}
-        />
-        <Input
-          {...register('shippingCity')}
-          placeholder="City"
-          id="shippingCity"
-          error={errors.shippingCity}
-        />
-        <Input
-          {...register('shippingStreet')}
-          placeholder="Stret"
-          id="shippingStreet"
-          error={errors.shippingStreet}
-        />
-        <Input
-          {...register('shippingHouseNumber')}
-          placeholder="HouseNumber"
-          id="shippingHouseNumber"
-          error={errors.shippingHouseNumber}
-        />
-        <Input
-          {...register('shippingApartment')}
-          placeholder="Apartment"
-          id="shippingApartment"
-          error={errors.shippingApartment}
-        />
-        <Input
-          {...register('shippingPostcode')}
-          placeholder="Postcode"
-          id="shippingPostcode"
-          error={errors.shippingPostcode}
-        />
-
-        <Button className={styles.button}>Регистрация</Button>
-
-        <div className={styles.submit}>
-          <div className={styles.accaunt}>Уже есть аккаунт?</div>
-          <Link className={styles.login} to="/login">
-            Войдите
-          </Link>
+          <Input
+            {...register('email', loginValidation())}
+            placeholder="Enter email"
+            type="text"
+            error={errors.email}
+          />
+          <Input
+            {...register('registerPassword', passwordValidation())}
+            placeholder="Enter password"
+            type="password"
+            error={errors.registerPassword}
+          />
+          <Input
+            {...register(
+              'repeatPassword',
+              repeatPasswordValidation(passwordValue),
+            )}
+            placeholder="Repeat password"
+            type="password"
+            error={errors.repeatPassword}
+          />
         </div>
-      </form>
-      <div className={styles.picbox}></div>
-    </div>
+
+        <div className={styles.container__billing}>
+          <h4 className={styles.info__title}>Адрес для выставления счетов</h4>
+          <Input
+            {...register('billingCountry', generalValidation())}
+            placeholder="Country"
+            id="country"
+            error={errors.billingCountry}
+          />
+          <Input
+            {...register('billingCity', generalValidation())}
+            placeholder="City"
+            id="billingCity"
+            error={errors.billingCity}
+          />
+          <Input
+            {...register('billingStreet', generalValidation())}
+            placeholder="Stret"
+            id="billingStreet"
+            error={errors.billingStreet}
+          />
+          <Input
+            {...register('billingHouseNumber', generalValidation())}
+            placeholder="HouseNumber"
+            id="billingHouseNumber"
+            error={errors.billingHouseNumber}
+          />
+          <Input
+            {...register('billingApartment', generalValidation())}
+            placeholder="Apartment"
+            id="billingApartment"
+            error={errors.billingApartment}
+          />
+          <Input
+            {...register('billingPostcode', generalValidation())}
+            placeholder="Postcode"
+            id="billingPostcode"
+            error={errors.billingPostcode}
+          />
+        </div>
+
+        <div className={styles.container__shipping}>
+          <h4 className={styles.info__title}>Адрес доставки</h4>
+          <Input
+            {...register('shippingCountry', generalValidation())}
+            placeholder="Country"
+            id="bilingCountry"
+            error={errors.shippingCountry}
+          />
+          <Input
+            {...register('shippingCity', generalValidation())}
+            placeholder="City"
+            id="shippingCity"
+            error={errors.shippingCity}
+          />
+          <Input
+            {...register('shippingStreet', generalValidation())}
+            placeholder="Stret"
+            id="shippingStreet"
+            error={errors.shippingStreet}
+          />
+          <Input
+            {...register('shippingHouseNumber', generalValidation())}
+            placeholder="HouseNumber"
+            id="shippingHouseNumber"
+            error={errors.shippingHouseNumber}
+          />
+          <Input
+            {...register('shippingApartment', generalValidation())}
+            placeholder="Apartment"
+            id="shippingApartment"
+            error={errors.shippingApartment}
+          />
+          <Input
+            {...register('shippingPostcode', generalValidation())}
+            placeholder="Postcode"
+            id="shippingPostcode"
+            error={errors.shippingPostcode}
+          />
+        </div>
+      </div>
+
+      <Button className={styles.button}>Регистрация</Button>
+
+      <div className={styles.submit}>
+        <div className={styles.accaunt}>Уже есть аккаунт?</div>
+        <Link className={styles.login} to="/login">
+          Войдите
+        </Link>
+      </div>
+    </form>
   );
 };
 
---
+++ src/validators/general-validation.ts
@@ -0,0 +1,7 @@
+export const generalValidation = () => ({    
+    required: {
+      value: true,
+      message: 'Поле обязательно для заполнения!',
+    },
+  });
+  
