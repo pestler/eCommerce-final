602684c9ecc394976227e11e2660093db554e168
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-05-21 01:04:04 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
@@ -13,25 +10,25 @@ export default defineConfig({
       stream: 'stream-browserify',
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
-  },
+  // optimizeDeps: {
+  //   esbuildOptions: {
+  //     define: {
+  //       global: 'globalThis',
+  //     },
+  //     plugins: [
+  //       NodeGlobalsPolyfillPlugin({
+  //         process: true,
+  //         buffer: true,
+  //       }),
+  //       NodeModulesPolyfillPlugin(),
+  //     ],
+  //   },
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     plugins: [rollupNodePolyFill()],
+  //   },
+  // },
   test: {
     globals: true,
     environment: 'jsdom',
