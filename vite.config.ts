c1b6786aad369a340f399cc94a33e1b22be4a5c2
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-05-21 00:46:00 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import inject from '@rollup/plugin-inject';
+
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Inject global variables into the bundle
+    inject({
+      global: 'globalThis',
+    })
+  ],
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       define: {
@@ -24,7 +38,10 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       plugins: [
-          rollupNodePolyFill()
+        rollupNodePolyFill(),
+        inject({
+          global: 'globalThis', // Use globalThis as a polyfill for global in the Rollup build
+        })
       ],
     },
   },
