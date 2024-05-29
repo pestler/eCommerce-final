d99b4aaaa2caf9e31a298285e16a8bbade64163b
Svetlana <claire-An@mail.ru>
2024-05-29 23:42:02 +0500

    feat: add hooks

--- src/pages/productPage/ProductPage.tsx
+++ src/pages/productPage/ProductPage.tsx
@@ -1,4 +1,8 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import imghumidity from '../../assets/images/humidity.png';
+import imglightning from '../../assets/images/lightning.png';
+import imgtemperature from '../../assets/images/temperature.png';
+import { ProductDto } from '../../mappers/dto/product.dto';
 import { productMapper } from '../../mappers/product.mapper';
 import { productsService } from '../../services/product.service';
 import styles from './product.module.scss';
@@ -7,34 +11,21 @@ const descriptionProduct = [
   {
     name: 'lightning',
     title: 'Освещение',
+    src: imglightning,
   },
   {
     name: 'humidity',
     title: 'Влажность',
+    src: imghumidity,
   },
   {
     name: 'temperature',
     title: 'Температура',
+    src: imgtemperature,
   },
 ];
 
 const Product: React.FC = () => {
-  //   const [product, setProduct] = useState();
-
-  //   const fetchData = async () => {
-  //     const product = await getData();
-  //     if (product) setProduct(product);
-  //   };
-
-  //   fetchData();
-  //   useEffect( () => {
-  //    const fetchData = async () => {
-  //     const product = await getData();
-  //     setProduct(product);
-  //   };
-  //   fetchData();
-  // }, []);
-
   const getData = async (id: string) => {
     try {
       const { statusCode, body } = await productsService.getByID(id);
@@ -46,31 +37,40 @@ const Product: React.FC = () => {
     }
   };
 
-  const product = getData('0336d8f9-3243-49c7-a6fb-879f1f1e0609');
-  console.log(product);
+  const [product, setProduct] = useState<ProductDto>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const product = await getData('0336d8f9-3243-49c7-a6fb-879f1f1e0609');
+      setProduct(product);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.main__wrapper}>
-      <h2>{}</h2>
+      <h2>{product?.name}</h2>
       <div className={styles.product__content}>
-        {/* <img src={} className={styles.main__imgcenter} /> */}
+        <img src={product?.images[0].url} className={styles.main__imgcenter} />
         <div className={styles.product__description}>
           {descriptionProduct.map((val) => {
             return (
               <div className={styles.product__option}>
-                <img src={`${val.name}.png`} />
+                <img src={val.src} />
                 <div>
                   <h3>{val.title}</h3>
-                  <p className={styles.option__description}>{}</p>
+                  <p className={styles.option__description}>
+                    {product[val.name]}
+                  </p>
                 </div>
               </div>
             );
           })}
           <p className={styles.product__size}>
-            Высота: <span>{} см</span>
+            Высота: <span>{product?.height} см</span>
           </p>
           <p className={styles.product__size}>
-            Диаметр (горшка): <span>{} см</span>
+            Диаметр (горшка): <span>{product?.diameter} см</span>
           </p>
         </div>
       </div>
