41fafaa8cf2889f0dc5e6037815d6f12cb155350
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-06-01 17:46:54 +0300

    Update input search

--- src/components/inputSearch/InputSearch.tsx
+++ src/components/inputSearch/InputSearch.tsx
@@ -1,10 +1,24 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import styles from './inputSearch.module.scss';
 
+import Image from '../../assets/images/humidity.png'
+import {ProductProjectionInterface} from "../../interface/productProjection.interface.ts";
+
 const InputSearch: FC<React.HTMLProps<HTMLInputElement>> = ({ ...props }) => {
+  const [resultSearch, setResultSearch] = useState<ProductProjectionInterface[]>();
+
+  setResultSearch([])
+
+
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input className={styles.inputSearch} {...props} type="text" />
+        {resultSearch && <div className={styles.searchList}>
+        <div className={styles.searchListItem}>
+          <img src={Image} alt=""/>
+          <span>Название</span>
+        </div>
+      </div>}
     </div>
   );
 };
