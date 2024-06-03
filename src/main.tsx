d48e3888943c270908779ca09d833acc93b135eb
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-06-03 22:56:00 +0300

    Added include mui

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 
-import {Buffer} from 'buffer';
-import process from 'process';
-
-// sax.js needs these
-window.Buffer = Buffer;
-window.process = process;
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
