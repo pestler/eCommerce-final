abd84597bb5b15744bb647e047be206a1b4e16b7
Svetlana <claire-An@mail.ru>
2024-06-03 20:37:11 +0500

    fix: fix render product page

--- src/mappers/product.mapper.ts
+++ src/mappers/product.mapper.ts
@@ -5,21 +5,6 @@ class ProductMapper {
   public fromDto(data: ProductData): ProductDto {
     const product: ProductDto = {
       name: data.name['ru-BY'],
-      lightning: data.masterVariant.attributes
-        ? data.masterVariant.attributes[0].value
-        : '',
-      humidity: data.masterVariant.attributes
-        ? data.masterVariant.attributes[1].value
-        : '',
-      temperature: data.masterVariant.attributes
-        ? data.masterVariant.attributes[2].value
-        : '',
-      height: data.masterVariant.attributes
-        ? data.masterVariant.attributes[3].value
-        : '',
-      diameter: data.masterVariant.attributes
-        ? data.masterVariant.attributes[4].value
-        : '',
       images: data.masterVariant.images?.length
         ? data.masterVariant.images
         : [],
@@ -35,6 +20,11 @@ class ProductMapper {
       const price = priceProduct[0].value.centAmount.toString();
       product.price.centAmount = +price.slice(0, -2);
     }
+
+    data.masterVariant.attributes?.forEach((attr) => {
+      product[attr.name] = attr.value
+    })
+
     return product;
   }
 }
