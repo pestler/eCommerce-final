1b005ae70a02e0fbbe2f5b9353bd44fa737a12b5
Svetlana <claire-An@mail.ru>
2024-06-04 22:02:57 +0500

    feat: add slider modal

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -4,22 +4,25 @@ import styles from './slider.module.scss';
 
 type PropsSlider = React.HTMLProps<HTMLElement> & {
   images: Image[];
+  currentSlide: number;
+  handleOpenModal(e: React.MouseEvent<HTMLElement>): void;
 };
 
-const SliderSimple = ({ images }: PropsSlider) => {
+const SliderSimple = ({ images, currentSlide, handleOpenModal }: PropsSlider) => {
   const settings = {
     dots: false,
     infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    initialSlide: currentSlide,
   };
   return (
     <Slider {...settings}>
       {images.map((val, index) => {
         return (
           <div className={styles.slider__item} key={`slider-img-${index}`}>
-            <img src={val.url} className={styles.slider__image} />
+            <img src={val.url} className={styles.slider__image} onClick={handleOpenModal}/>
           </div>
         );
       })}
