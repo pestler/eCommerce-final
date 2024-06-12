b639bba2e796578b871f59239ae600440e04157f
Aleh Kuis <pestleralex@gmail.com>
2024-06-12 19:55:52 +0300

    feat: add link and effect

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -17,12 +17,20 @@ const About: React.FC = () => {
         <h1>О нашей команде!</h1>
         {aboutTitle}
         <div className={styles.aboutRegardsContainer}>
-          <h2 className={styles.aboutRegards}>Большое спасибо школе RS School</h2>
+          <div className={styles.containerEffect}>
+          <a            
+            href="https://rollingscopes.com/"
+            target="_blank"    
+            className={styles.aboutRsButtonH2}        
+          >
+            <h2 className={styles.aboutRegards}>Большое спасибо школе RS School</h2>
+            </a>
           <a
             href="https://rs.school/"
             target="_blank"
             className={styles.aboutRsButton}
-          ></a>
+          ></a>  
+          </div>          
         </div>
       </div>
       <div className={styles.developer__container}>
