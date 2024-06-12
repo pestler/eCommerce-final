fa318f9734e33eaaa54ca465ec3f0f0380a2a152
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-06-12 23:58:05 +0300

    Fixed burger menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -86,7 +86,10 @@ const Header: React.FC = () => {
               )}
               <BasicMenu
                 buttonContent={!isAuthenticated ? <LoginSvg /> : <ProfileSvg />}
-                menuItems={eventsMenu.filter((item: string) =>
+                menuItems={
+                eventsMenu
+                    .filter((item) => item !== 'Каталог' && item !== 'Главная')
+                    .filter((item: string) =>
                   isAuthenticated
                     ? item === 'Выйти' || item === 'Профиль'
                     : item !== 'Выйти' && item !== 'Профиль',
