66666038e26d6d972267d787f02f7751a9e0300b
Aleh Kuis <pestleralex@gmail.com>
2024-06-17 13:00:20 +0300

    feat: del empty files

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,19 +6,7 @@ import { describe, it } from 'vitest';
 describe('App', () => {
   it('renders headline', () => {
     render(<App />);
-
-    screen.debug();
-
-    // check if App components renders headline
+    screen.debug();    
   });
 });
 
-/* import React from 'react';
-import ReactDOM from 'react-dom';
-import MoviesSingle from './../src/components/footer/Footer';
-import { it } from 'vitest';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<MoviesSingle />, div);
-}); */
