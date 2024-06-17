ca0f9b8feb9f7ce3eee647302fdb5e973b74fe45
Aleh Kuis <pestleralex@gmail.com>
2024-06-17 12:24:37 +0300

    feat: add App test

--- test-setup/setup.ts
+++ test-setup/setup.ts
@@ -1,8 +1,8 @@
 import Enzyme from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
-import { expect, afterEach } from 'vitest';
+/* import { expect, afterEach } from 'vitest';
 import matchers from '@testing-library/jest-dom/matchers';
-import { cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react'; */
 
 Enzyme.configure({ adapter: new Adapter() });
 //expect.extend(matchers);
